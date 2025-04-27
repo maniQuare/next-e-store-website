d9cd5ecbcd892b0067b27a460c41e5f6f16998e6
maniquare <maniquare@gmail.com>
2025-04-27 13:33:56 +0530

    Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,93 @@
-"use client"
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
 
-export default function Banner(){
+import styles from "@/styles/Banner.module.css";
+import { GrSecure } from "react-icons/gr";
+import { IoFastFood } from "react-icons/io5";
+import { GiFoodTruck } from "react-icons/gi";
+
+const introImages = [
+  "/women/women2.jpg",
+  "/women/women3.jpg",
+  "/women/women4.jpg",
+ 
+];
+export default function Banner() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("interval");
+      setCurrentSlide((currentValue) => {
+        if (currentValue == introImages.length - 1) {
+          return 0;
+        } else {
+          return currentValue + 1;
+        }
+      });
+    }, 3000);
+    return () => {
+      console.log("interval clean up");
+      clearInterval(interval); // Clean up - Inv
+    };
+  }, []);
+
+
   return (
-    <div>Banner</div>
-  )
+    <div className="min-h-[550px] flex justify-center items-center py-12 sm:py-0">
+      <div className="container">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 items-center">
+          {/* image section */}
+          <div data-aos="zoom-in">
+            <div className={styles.slider}>
+              {introImages.map((images: any, index: any) => {
+                return (
+                  <img
+                    key={"Slide" + index}
+                    className={`${styles.slide} ${currentSlide === index && styles.active} max-w-[500px] h-[350px] w-full mx-auto drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)] object-cover `}
+                    src={images}
+                    alt={"Slide" + index}
+                  
+                  />
+                );
+              })}
+            </div>
+          </div>
+
+          {/* text details section */}
+          <div className="flex flex-col justify-center gap-6 sm:pt-0">
+            <h1 data-aos="fade-up" className="text-3xl sm:text-4xl font-bold">
+              Winter Sale upto 50% Off
+            </h1>
+            <p
+              data-aos="fade-up"
+              className="text-sm text-gray-500 tracking-wide leading-5"
+            >
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
+              reiciendis inventore iste ratione ex alias quis magni at optio
+            </p>
+            <div className="flex flex-col gap-4">
+              <div data-aos="fade-up" className="flex items-center gap-4">
+                <GrSecure className="text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-violet-100 dark:bg-violet-400" />
+                <p>Quality Products</p>
+              </div>
+              <div data-aos="fade-up" className="flex items-center gap-4">
+                <IoFastFood className="text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-orange-100 dark:bg-orange-400" />
+                <p>Fast Delivery</p>
+              </div>
+              <div data-aos="fade-up" className="flex items-center gap-4">
+                <GiFoodTruck className="text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-green-100 dark:bg-green-400" />
+                <p>Easy Payment method</p>
+              </div>
+              <div data-aos="fade-up" className="flex items-center gap-4">
+                <GiFoodTruck className="text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-yellow-100 dark:bg-yellow-400" />
+                <p>Get Offers</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
